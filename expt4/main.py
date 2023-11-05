ddae7b62597138f50b5a4f47942ce46d1d5258f0
Chandu Bayyavarapu <116875885+Chandu106@users.noreply.github.com>
2023-11-05 19:01:21 +0530

    Add files via upload

---
+++ expt4/main.py
@@ -0,0 +1,33 @@
+#4a
+def return_cost(cost_dict, flavor):
+    if flavor in cost_dict:
+        return cost_dict[flavor]
+    else:
+        return 0
+flavor_cost_dict = {
+    "biriyani": 250,
+    "chicken curry": 200,
+    "vanilla icecreame": 225
+}
+flavor = "biriyani"
+cost = return_cost(flavor_cost_dict, flavor)
+print("The cost of" ,flavor, "is", cost)
+flavor = "mint"
+cost = return_cost(flavor_cost_dict, flavor)
+print("The cost of" ,flavor, "is", cost)
+
+#4b
+def chararacter_count():
+  string_input=str(input())
+  b=[]
+  c=[]
+  for i in range (len(string_input)):
+    d=0
+    if string_input[i] not in b:
+      b.append(string_input[i])
+      d=string_input.count(string_input[i])
+      c.append(d)
+
+  result=dict(zip(b,c))
+  print(result)
+chararacter_count()
